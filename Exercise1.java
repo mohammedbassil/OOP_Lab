e78bed0b1c39cb7945c13511a49fba7ebe536fa7
mohammedbassil <mohammedbassil966@gmail.com>
2025-07-30 02:11:47 -0700

    Create Exercise1.java

---
+++ Exercise1.java
@@ -0,0 +1,25 @@
+package oopslab;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		int sum=0;
+		int start=1;
+		
+		
+		for (int i=1;i<=100;i++) {
+			sum+=i;
+			
+		}
+
+		
+
+		
+			System.out.println("the sum of n numbers from the 1 to 100:"+sum);
+
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+
